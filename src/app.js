2fa4ec08c975efc899e4316cb5b57a88900b55ee
piecioshka <piecioshka@gmail.com>
2013-04-14 14:47:36 +0200

    add unhashing fn - first version of application is ready. You can use to sending crypt message. Alsa to uncrypt sue the same version of app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 (function () {
   "use strict";
 
-  function $(selector) {
-    return document.querySelector(selector);
+  function $$(selector) {
+    return document.querySelectorAll(selector);
   }
 
-  var form = $("form");
-  var plain_text = $("#plain-text");
-  var cifer_text= $("#cifer-text");
-  var key = $("#key");
+  function set_crypt(nr, callback) {
+    var form = $$(".form")[nr];
+    var plaintext = $$(".plain-text")[nr];
+    var ciphertext = $$(".cifer-text")[nr];
+    var key = $$(".key")[nr];
 
-  form.addEventListener("submit", function (evt) {
-    var p = plain_text.value;
-    var k = +key.value;
-    cifer_text.value = hash(p, k);
-    evt.preventDefault();
-  });
+    form.addEventListener("submit", function (evt) {
+      var p = plaintext.value;
+      var k = +key.value;
+      ciphertext.value = callback(p, k);
+      evt.preventDefault();
+    });
+  }
+
+  function set_decrypt(nr, callback) {
+    var form = $$(".form")[nr];
+    var plaintext = $$(".plain-text")[nr];
+    var ciphertext = $$(".cifer-text")[nr];
+    var key = $$(".key")[nr];
+
+    form.addEventListener("submit", function (evt) {
+      var p = ciphertext.value;
+      var k = +key.value;
+      plaintext.value = callback(p, k);
+      evt.preventDefault();
+    });
+  }
+
+  set_crypt(0, hash);
+  set_decrypt(1, unhash);
 
 }(this));
 

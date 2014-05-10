e8283e7802a6be51608e3caabc97956433172674
Piotr Kowalski <piecioshka@gmail.com>
2014-05-10 23:53:27 +0200

    Add error alerts

--- scripts/app.js
+++ scripts/app.js
@@ -14,52 +14,77 @@
 
     App.prototype = {
         initialize: function () {
-            this.set_crypt(0, this.hash);
-            this.set_decrypt(1, this.unhash);
+            this._set_crypt(0, this.hash);
+            this._set_decrypt(1, this.unhash);
         },
 
+        _checkParams: function (v) {
+            var errors = [];
+            var status;
+
+            if (!(/^\d+$/).test(v)) {
+                errors.push('Incorrect value param');
+            }
+
+            if (_.size(errors)) {
+                alert(_.first(errors));
+            }
+
+            // update status by error list
+            status = !_.size(errors);
+
+            return status;
+        },
+
+
         hash: function (p, k) {
-            var s = p.split("");
+            var s = p.split('');
             s = _.map(s, function(l) {
                 var c = l.charCodeAt(0) + k;
                 return String.fromCharCode(c);
             });
-            return s.join("");
+            return s.join('');
         },
 
         unhash: function (p, k) {
-            var s = p.split("");
+            var s = p.split('');
             s = _.map(s, function(l) {
                 var c = l.charCodeAt(0) - k;
                 return String.fromCharCode(c);
             });
-            return s.join("");
+            return s.join('');
         },
 
-        set_crypt: function (nr, callback) {
-            var form = $(".form")[nr];
-            var plaintext = $(".plain-text")[nr];
-            var ciphertext = $(".cifer-text")[nr];
-            var key = $(".key")[nr];
+        _set_crypt: function (nr, callback) {
+            var self = this;
+            var form = $('.form')[nr];
+            var plaintext = $('.plain-text')[nr];
+            var ciphertext = $('.cifer-text')[nr];
+            var key = $('.key')[nr];
 
-            form.addEventListener("submit", function (evt) {
+            $(form).on('submit', function (evt) {
                 var p = plaintext.value;
                 var k = +key.value;
-                ciphertext.value = callback(p, k);
+                if (self._checkParams(k)) {
+                    ciphertext.value = callback(p, k);
+                }
                 evt.preventDefault();
             });
         },
 
-        set_decrypt: function (nr, callback) {
-            var form = $(".form")[nr];
-            var plaintext = $(".plain-text")[nr];
-            var ciphertext = $(".cifer-text")[nr];
-            var key = $(".key")[nr];
+        _set_decrypt: function (nr, callback) {
+            var self = this;
+            var form = $('.form')[nr];
+            var plaintext = $('.plain-text')[nr];
+            var ciphertext = $('.cifer-text')[nr];
+            var key = $('.key')[nr];
 
-            form.addEventListener("submit", function (evt) {
+            $(form).on('submit', function (evt) {
                 var p = ciphertext.value;
                 var k = +key.value;
-                plaintext.value = callback(p, k);
+                if (self._checkParams(k)) {
+                    plaintext.value = callback(p, k);
+                }
                 evt.preventDefault();
             });
         }

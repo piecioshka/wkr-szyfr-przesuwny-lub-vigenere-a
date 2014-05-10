1963628a1b0240c6516bebb0624bf43c7d0718a1
Piotr Kowalski <piecioshka@gmail.com>
2014-05-10 16:06:55 +0200

    Convert hashing.pl to `wkr-szyfr-cezara`

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-hashing.pl
-=============
+# Wstęp do kryptologii - szyfr Cezara
 
-Simple coding plaintext to ciphertext for subject: `"Wstęp do kryptologii"`
+Mini aplikacja zaszyfrować teskt z użyciem szyfru Cezara
+
+## Demo
+
+[http://jsninja.pl/wkr-szyfr-cezara/][1]
+
+## License
+
+[The MIT License][2]
+
+
+[1]: http://jsninja.pl/wkr-szyfr-cezara/
+[2]: http://piecioshka.mit-license.org/
--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,70 @@
-(function () {
-  "use strict";
+/**
+ * @author Piotr Kowalski <piecioshka@gmail.com>
+ * @see http://jsninja.pl/wkr-szyfr-cezara/
+ * @licence The MIT License {@link http://piecioshka.mit-license.org/}
+ */
+(function (root, factory) {
+    root.App = factory(root._);
+}(this, function (_) {
+    'use strict';
 
-  function $$(selector) {
-    return document.querySelectorAll(selector);
-  }
+    function App() {
+        this.initialize();
+    }
 
-  function set_crypt(nr, callback) {
-    var form = $$(".form")[nr];
-    var plaintext = $$(".plain-text")[nr];
-    var ciphertext = $$(".cifer-text")[nr];
-    var key = $$(".key")[nr];
+    App.prototype = {
+        initialize: function () {
+            this.set_crypt(0, this.hash);
+            this.set_decrypt(1, this.unhash);
+        },
 
-    form.addEventListener("submit", function (evt) {
-      var p = plaintext.value;
-      var k = +key.value;
-      ciphertext.value = callback(p, k);
-      evt.preventDefault();
-    });
-  }
+        hash: function (p, k) {
+            var s = p.split("");
+            s = _.map(s, function(l) {
+                var c = l.charCodeAt(0) + k;
+                return String.fromCharCode(c);
+            });
+            return s.join("");
+        },
 
-  function set_decrypt(nr, callback) {
-    var form = $$(".form")[nr];
-    var plaintext = $$(".plain-text")[nr];
-    var ciphertext = $$(".cifer-text")[nr];
-    var key = $$(".key")[nr];
+        unhash: function (p, k) {
+            var s = p.split("");
+            s = _.map(s, function(l) {
+                var c = l.charCodeAt(0) - k;
+                return String.fromCharCode(c);
+            });
+            return s.join("");
+        },
 
-    form.addEventListener("submit", function (evt) {
-      var p = ciphertext.value;
-      var k = +key.value;
-      plaintext.value = callback(p, k);
-      evt.preventDefault();
-    });
-  }
+        set_crypt: function (nr, callback) {
+            var form = $(".form")[nr];
+            var plaintext = $(".plain-text")[nr];
+            var ciphertext = $(".cifer-text")[nr];
+            var key = $(".key")[nr];
 
-  set_crypt(0, hash);
-  set_decrypt(1, unhash);
+            form.addEventListener("submit", function (evt) {
+                var p = plaintext.value;
+                var k = +key.value;
+                ciphertext.value = callback(p, k);
+                evt.preventDefault();
+            });
+        },
 
-}(this));
+        set_decrypt: function (nr, callback) {
+            var form = $(".form")[nr];
+            var plaintext = $(".plain-text")[nr];
+            var ciphertext = $(".cifer-text")[nr];
+            var key = $(".key")[nr];
+
+            form.addEventListener("submit", function (evt) {
+                var p = ciphertext.value;
+                var k = +key.value;
+                plaintext.value = callback(p, k);
+                evt.preventDefault();
+            });
+        }
+    };
+
+    return App;
+}));
 
